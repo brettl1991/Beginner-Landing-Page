e67362849f806e99aab1a8e45ccacb41298af363
Agnes <brettl1991@gmail.com>
2021-08-31 20:40:24 +0100

    Adding scrolling to the mobile menu when we open and remove when we close plus fixing typo

--- app/js/script.js
+++ app/js/script.js
@@ -1,4 +1,5 @@
 const btnHamburger = document.querySelector("#btnHamburger");
+const body = document.querySelector("body");
 const header = document.querySelector(".header");
 const overlay = document.querySelector(".overlay");
 const fadeElems = document.querySelectorAll(".has-fade");
@@ -8,14 +9,15 @@ btnHamburger.addEventListener("click", function () {
 
   if (header.classList.contains("open")) {
     //Close Hamburger Menu
+    body.classList.remove("noscroll");
     header.classList.remove("open");
-    fadeElems.forEach(function(element){
+    fadeElems.forEach(function (element) {
       element.classList.remove("fade-in");
       element.classList.add("fade-out");
-    })
-   
+    });
   } else {
-    //Close Hamburger Menu
+    //Open Hamburger Menu
+    body.classList.add("noscroll");
     header.classList.add("open");
     fadeElems.forEach(function (element) {
       element.classList.remove("fade-out");
